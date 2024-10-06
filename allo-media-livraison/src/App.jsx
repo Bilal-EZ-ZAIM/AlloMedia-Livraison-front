b414eb552ffa75170ccdc992d584f50840f40c3a
Bilal-EZ-ZAIM <bilalzail499@gmail.com>
2024-10-06 12:20:47 +0100

    ALF-4 Créer le composant de demande de réinitialisation.

--- allo-media-livraison/src/App.jsx
+++ allo-media-livraison/src/App.jsx
@@ -7,6 +7,7 @@ import Header from "./commeptes/Header";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import VerfieOtp from "./pages/VerfieOtp";
+import ForgetPassword from "./pages/ForgetPassword";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -20,6 +21,7 @@ function App() {
           <Route path="/signin" element={<Login />}></Route>
           <Route path="/signup" element={<Register />}></Route>
           <Route path="/verfei" element={<VerfieOtp />}></Route>
+          <Route path="/forget-password" element={<ForgetPassword />}></Route>
         </Routes>
       </BrowserRouter>
     </>
